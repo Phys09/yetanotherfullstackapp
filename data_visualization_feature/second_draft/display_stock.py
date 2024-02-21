0b7aec03837a96e8ecc8ecca4fd5ea1f4d340d52
agiatikazinat <151879446+agiatikazinat@users.noreply.github.com>
2024-02-21 17:37:16 -0500

    Update display_stock.py

--- data_visualization_feature/second_draft/display_stock.py
+++ data_visualization_feature/second_draft/display_stock.py
@@ -7,6 +7,6 @@ import requests
 import pandas as pd            
 import finnhub
 
-finnhub_client = finnhub.Client(api_key = "cnb6pm1r01qks5iv4orgcnb6pm1r01qks5iv4os0")
+finnhub_client = finnhub.Client(api_key = "")
 
 print(finnhub_client.marke)
